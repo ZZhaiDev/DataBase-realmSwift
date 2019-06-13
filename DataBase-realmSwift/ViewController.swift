011d062f3b202e8c37b7c1eccc44c4507123c3dc
Zijia Zhai <zijiazhai@gmail.com>
2019-06-13 14:28:22 -0500

    init

---
+++ DataBase-realmSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataBase-realmSwift
+//
+//  Created by Zijia Zhai on 6/13/19.
+//  Copyright © 2019 BMLF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
